8cbc74ce341bac48ab7cf4236b3078b2af2f2dcd
sirironheart <127705750+Siriron@users.noreply.github.com>
2026-07-15 14:00:56 +0600

    Add files via upload

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,11 @@ interface ImportMetaEnv {
   readonly VITE_CONTRACT_ADDRESS: string
   readonly VITE_GENLAYER_RPC_URL: string
   readonly VITE_EXPLORER_URL: string
+  readonly VITE_DEPLOY_TX: string
+  readonly VITE_CONTRACT_ADDRESS_BRADBURY: string
+  readonly VITE_GENLAYER_RPC_URL_BRADBURY: string
+  readonly VITE_EXPLORER_URL_BRADBURY: string
+  readonly VITE_DEPLOY_TX_BRADBURY: string
 }
 
 interface ImportMeta {
